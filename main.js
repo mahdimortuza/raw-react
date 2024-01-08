6f91dcc46321abc538165677773fb6f23a128102
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-08 09:09:09 +0600

    writing first jsx

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-const HelloWorld = () => {
-    return React.createElement('h2', {}, "Hello world.")
-}
+// const HelloWorld = () => {
+//     return React.createElement('h2', {}, "Hello world.")
+// }
 
-const App = () => {
-    {
-       return React.createElement('h1', {style:{color: "red"}}, "This is the fokkira element.", HelloWorld())
-    }
-} 
+// const App = () => {
+//     {
+//        return React.createElement('h1', {style:{color: "red"}}, "This is the fokkira element.", HelloWorld())
+//     }
+// } 
+
+
+const HelloWorld = () => {
+    return (
+        <React.Fragment>
+        <h1>Hello world</h1>
+        <p>this is a website </p>
+        </React.Fragment>
+    )
+}
+const App =() => {
+    return <div style ={{color: "red"}}> this is a fokira app</div>
+}
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(React.createElement(App))
+root.render(<HelloWorld />)

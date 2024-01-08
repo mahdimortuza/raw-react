24823a89a9f7317cfcd9577c30d1022cdbed97dd
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-08 09:03:06 +0600

    adding babel

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const App = React.createElement('h1', {style:{color: "red"}}, "hello world")
+const HelloWorld = () => {
+    return React.createElement('h2', {}, "Hello world.")
+}
+
+const App = () => {
+    {
+       return React.createElement('h1', {style:{color: "red"}}, "This is the fokkira element.", HelloWorld())
+    }
+} 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(App)
+root.render(React.createElement(App))
